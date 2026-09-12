7529e83e9b5a338e016d6ae3c4efdee05c0d0bfe
Minh <petertruong.bruh@gmail.com>
2026-09-12 13:47:06 +0700

    added fast setup for both windows and mac, update README.md

--- setup.bat
+++ setup.bat
@@ -5,7 +5,7 @@ set /p IP_ADDRESS=Enter your IP address:
 echo IP_ADDRESS=%IP_ADDRESS%> .env
 echo VITE_IP_ADDRESS=%IP_ADDRESS%> frontend\.env
 
-start cmd /k "cd /d %~dp0 && pip install -r requirements.txt"
+start cmd /k "cd /d %~dp0 && python -m pip install -r requirements.txt"
 start cmd /k "cd /d %~dp0frontend && npm install"
 
 echo "Setup complete."
