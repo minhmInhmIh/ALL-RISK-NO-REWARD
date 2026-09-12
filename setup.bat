dc4bad78f992e723eb30bb24da204e3e001d0218
Minh <petertruong.bruh@gmail.com>
2026-09-12 13:27:03 +0700

    added setup.bat for fast setup

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set /p IP_ADDRESS=Enter your IP address: 
+
+echo IP_ADDRESS=%IP_ADDRESS%> .env
+echo VITE_IP_ADDRESS=%IP_ADDRESS%> frontend\.env
+
+start cmd /k "cd /d %~dp0 && pip install -r requirements.txt"
+start cmd /k "cd /d %~dp0frontend && npm install"
+
+exit
